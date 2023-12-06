7d3ffedad9e68a7a33079477ef1f2d97c4d4f3c4
Cheyenne <sterbickc@gmail.com>
2023-12-05 23:44:22 -0700

    VI: tuned and trained track W for VI

--- driver.py
+++ driver.py
@@ -1,17 +1,17 @@
 import pandas as pd
 from Agent import Agent
 
-# qlearner = Agent("./tracks/L-Track/L-track-2.txt")
-# QTable = qlearner.qLearning()
+qlearner = Agent("./tracks/L-Track/L-track-2.txt")
+QTable = qlearner.qLearning()
 
 
-def tuneVI():
-    bellmanErrors = [0.1, 0.2, 0.3, 0.4, 0.5]
-    discounts = [0.8, 0.85, 0.9, 0.95, 1.0]
+# def tuneVI():
+#     bellmanErrors = [0.1, 0.2, 0.3, 0.4, 0.5]
+#     discounts = [0.8, 0.85, 0.9, 0.95, 1.0]
 
-    for bE in bellmanErrors:
-        for d in discounts:
-            vIter = Agent("./tracks/W-Track/W-track-full.txt")
-            values = vIter.valueIteration(bE, d)
+#     for bE in bellmanErrors:
+#         for d in discounts:
+#             vIter = Agent("./tracks/W-Track/W-track-full.txt")
+#             values = vIter.valueIteration(bE, d)
 
-tuneVI()
+# tuneVI()
